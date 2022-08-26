eb303b5b0577530644ff99a7811a441b8f8bc3fe
Javier Roman <67432563+javierromancsa@users.noreply.github.com>
2022-08-26 07:03:16 -0500

    locker storage name

--- scenarios/deeplearning/code/script/createclustertemp.sh
+++ scenarios/deeplearning/code/script/createclustertemp.sh
@@ -4,6 +4,6 @@
 wget  https://bmhpcwus2.blob.core.windows.net/share/cc-slurm/slurm-custom-v0.6.tgz
 tar -xzvf slurm-custom-v0.6.tgz
 cd slurm-custom
-/usr/local/bin/cyclecloud project upload
+/usr/local/bin/cyclecloud project upload azure-storage
 cd templates
 /usr/local/bin/cyclecloud import_template slurm-ngc -f ./slurm-custom.txt -c slurm
