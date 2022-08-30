f0b5f24290500633caf2d56fccd9c0985d6659d2
javierromancsa <jaroma@microsoft.com>
2022-08-30 18:19:01 -0500

    consolidate all dependecies on 1 tar file and modified creates script

--- scenarios/deeplearning/code/script/createclustertemp.sh
+++ scenarios/deeplearning/code/script/createclustertemp.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 /usr/local/bin/cyclecloud initialize --batch --force --url=https://localhost:443 --verify-ssl=false --username=$1 --password=$2
-wget  https://bmhpcwus2.blob.core.windows.net/share/cc-slurm/slurm-custom-v0.6.tgz
+wget https://raw.githubusercontent.com/Azure/HPC-Accelerator/main/scenarios/deeplearning/code/script/slurm-custom-v0.6.m.tgz
 tar -xzvf slurm-custom-v0.6.tgz
-wget https://raw.githubusercontent.com/Azure/HPC-Accelerator/main/scenarios/deeplearning/code/script/002-docker-setup.sh
-mv 002-docker-setup.sh slurm-custom/specs/ndv4/cluster-init/scripts/
 cd slurm-custom
 /usr/local/bin/cyclecloud project upload azure-storage
 cd templates
