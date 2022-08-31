6e6574f8fdf06576f459e36e1c536c1099e33091
Javier Roman <67432563+javierromancsa@users.noreply.github.com>
2022-08-30 19:04:16 -0500

    Update createclustertemp.sh
    fix typo on script

--- scenarios/deeplearning/code/script/createclustertemp.sh
+++ scenarios/deeplearning/code/script/createclustertemp.sh
@@ -2,7 +2,7 @@
 
 /usr/local/bin/cyclecloud initialize --batch --force --url=https://localhost:443 --verify-ssl=false --username=$1 --password=$2
 wget https://raw.githubusercontent.com/Azure/HPC-Accelerator/main/scenarios/deeplearning/code/script/slurm-custom-v0.6.m.tgz
-tar -xzvf slurm-custom-v0.6.tgz
+tar -xzvf slurm-custom-v0.6.m.tgz
 cd slurm-custom
 /usr/local/bin/cyclecloud project upload azure-storage
 cd templates
