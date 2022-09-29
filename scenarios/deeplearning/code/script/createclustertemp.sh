53506d08e8ffecaa5abede33836b77493a2c793f
Javier Roman <67432563+javierromancsa@users.noreply.github.com>
2022-09-28 22:44:12 -0500

    Javier03 (#6)
    * add formating
    
    * adding scripts
    
    * add ubuntu marketplace
    
    * change after code update
    
    * Add files via upload
    
    * add nccl test ND40rs_v2
    
    * new pic
    
    * going back to main (#5)
    
    * fix the script
    
    * Javier03 (#4)
    
    * add formating
    
    * adding scripts
    
    * add ubuntu marketplace
    
    * change after code update
    
    * Add files via upload
    
    * add nccl test ND40rs_v2
    
    * new pic
    
    Co-authored-by: javierromancsa <jaroma@microsoft.com>
    
    Co-authored-by: javierromancsa <jaroma@microsoft.com>
    
    * Create readme
    
    * Rename readme to readme.md
    
    * Add files via upload
    
    * add 2
    
    * rename
    
    * add 2
    
    * add 3
    
    * Update HPC Skilling Hands-On Lab NDv4.md
    
    * Update HPC Skilling Hands-On Lab NDv4.md
    
    fix cmd to fetch VMID
    
    * Add files via upload
    
    * Add files via upload
    
    * Update HPC Skilling Hands-On Lab NDv4.md
    
    * Update simpleslurmjob.sh
    
    Co-authored-by: javierromancsa <jaroma@microsoft.com>

--- scenarios/deeplearning/code/script/createclustertemp.sh
+++ scenarios/deeplearning/code/script/createclustertemp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cyclecloud initialize --batch --force --url=https://localhost:443 --verify-ssl=false --username=$1 --password=$2
+<<<<<<< HEAD
+wget "https://jrslocker.blob.core.windows.net/repo/slurm-ndv4.v.1.tgz?sp=r&st=2022-09-26T05:11:09Z&se=2022-09-28T13:11:09Z&spr=https&sv=2021-06-08&sr=c&sig=ZElSlyDKo%2Fz%2BF3UCV9yUWBdTt9n8%2Bx8%2FQQOfNt0fEtY%3D" -k -O "slurm-ndv4.v.1.tgz"
+=======
 wget "https://jrslocker.blob.core.windows.net/repo/slurm-ndv4.v.1.tgz?sp=r&st=2022-09-28T22:21:58Z&se=2022-10-01T06:21:58Z&spr=https&sv=2021-06-08&sr=c&sig=u5D2gwsqOsS0%2B9lWFjUnMJoAeSpP95rAiPtu%2BromTtk%3D" -k -O "slurm-ndv4.v.1.tgz"
+>>>>>>> main
 tar -xzvf slurm-ndv4.v.1.tgz
 cd slurm-ndv4/projects/limits_1.0.0
 cyclecloud project upload azure-storage
@@ -25,4 +29,7 @@ sed -i "s/myregion/$4/g" slurmcycle.json
 cyclecloud import_cluster -f slurm_cycle.txt -p slurmcycle.json -c Ndv4Slurm -t
 sleep 2
 cyclecloud create_cluster Ndv4Slurm deeplearning -p slurmcycle.json
+<<<<<<< HEAD
+=======
 echo "CycleCloud UI IP:"; ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+'
+>>>>>>> main
